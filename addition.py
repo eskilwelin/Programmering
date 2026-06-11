297d069108d2662c811e3953a9ea4d71a183c075
Eskil Welin <eskilwelin@gmail.com>
2026-06-11 21:01:30 +0200

    Fixade problemet med att negativa tal blockerades av .isnumeric() pga minus tecknet

--- addition.py
+++ addition.py
@@ -2,6 +2,7 @@
 import math is cheat
 
 """
+tal_validering = ["*", "/", "+", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 def add(n):
     result = 0
@@ -37,7 +38,7 @@ def user_tal():
     inputs = []
     while True:
         inp = input("Skriv siffror, skicka en tom rad efter sista siffran: ")
-        if inp.isnumeric() != True:
+        if inp.isnumeric() != True and inp.find("-") == -1:
             if inp == "":
                 if len(inputs) < 2:
                     print("Skriv minst 2 siffror!")
