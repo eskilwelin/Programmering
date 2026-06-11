f0911f2f9a5541ae545453e0f9996ed585f37ea2
Eskil Welin <eskilwelin@gmail.com>
2026-06-11 18:16:41 +0200

    Funktioner version 1, inspirerad av FYFAN.py

--- addition.py
+++ addition.py
@@ -4,6 +4,23 @@ val = Input addition eller subtraktion
 
 """
 
+def addition(i, j):
+    return i + j
+
+def subtraktion(i, j):
+    return i - j
+
+def multiplikation(i, j):
+    return i * j
+
+def division(i, j):
+    if j == 0:
+        return "ERROR! Dividera inte med noll!"
+    else:
+        return i / j
+
+
+
 while True:
 
     val = str.lower(input("Välj 'addition', 'subtraktion', 'gånger' eller 'division': "))
@@ -12,28 +29,20 @@ while True:
         print("Felaktigt val, välj addition, gånger, division eller subtraktion")
         continue
 
-
     tal1 = int(input("Välj tal ett: "))
     tal2 = int(input("Välj tal två: "))
 
-    if val == "division" or "/" and tal2 == 0:
-        print("Dividera inte med 0 tack.")
-        continue
         
     if val == "addition" or val == "+":
-        addition = tal1 + tal2
-        print(f"Resultatet blir: {addition}")
+        print(f"Resultatet blir: {addition(tal1, tal2)}")
     elif val == "subtraktion" or val == "-":
-        subtraktion = tal1 - tal2
-        print(f"Resultatet blir: {subtraktion}")
+        print(f"Resultatet blir: {subtraktion(tal1, tal2)}")
     elif val == "gånger" or val == "*":
-        multiplikation = tal1 * tal2
-        print(f"Resultatet blir: {multiplikation}")
+        print(f"Resultatet blir: {multiplikation(tal1, tal2)}")
     else:
-        division = tal1 / tal2
-        print(f"Resultatet blir: {division}")
+        print(f"Resultatet blir: {division(tal1, tal2)}")
 
     again = str.lower(input("Vill du testa ett till tal? Ja / Nej "))
    
-    if again != "ja":
+    if again != "ja" and again != "j":
         exit()
