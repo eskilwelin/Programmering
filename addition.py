5308cb9ccde364a7000bb29a54a325d313bc220c
Eskil Welin <eskilwelin@gmail.com>
2026-06-11 22:40:09 +0200

    Clean code.

--- addition.py
+++ addition.py
@@ -4,7 +4,7 @@ import math is cheat
 
 DONE:
 - Konvertera till funktioner
-
+- User input blockerar negativa tal
 
 TODO: 
 - Fixa nästlade if satser med if not https://lawyerdev.medium.com/i-never-write-nested-ifs-e4e91a5440ee
@@ -38,51 +38,53 @@ def div(n):
     result = n[0]
     n.pop(0)
     if 0 in n:
-        return "ERROR! Dividera inte med noll!"
+        return "ERROR! Ingen division med 0!"
     for number in n:
         result /= number
     return result
 
 
-def user_tal():
-    inputs = []
+def user_num():
+    num_list = []
     while True:
-        inp = input("Skriv siffror, skicka en tom rad efter sista siffran: ")
-        if inp.isnumeric() != True and inp.find("-") == -1:
-            if inp == "":
-                if len(inputs) < 2:
-                    print("Skriv minst 2 siffror!")
+        user_input = input("Ange en siffra åt gången, skicka en tom rad efter sista siffran: ")
+        if not user_input.isnumeric() and user_input.find("-") == -1:
+            if user_input == "":
+                if len(num_list) < 2:
+                    print("Ange minst 2 siffror!")
                     continue
                 else:
                     break
-            print("Bara siffror tack!!!")
+            print("Ange enbart siffror.")
             continue
-        inputs.append(int(inp))
+        num_list.append(int(user_input))
     
-    return inputs
+    return num_list
 
 
 while True:
 
     operator = str.lower(input("Välj en operator + - / * : "))
 
-    if operator not in ["addition", "+", "subtraktion", "-", "gånger", "*", "division", "/"]:
-        print("Felaktigt val, välj + - / *")
+    if operator not in ["+", "-", "*", "/"]:
+        print("Felaktigt val, välj + - / * ")
         continue
 
-
-    if operator == "addition" or operator == "+":
-        result = add(user_tal())
+    if operator == "+":
+        result = add(user_num())
         print(f"Resultatet blir: {result}")
-    elif operator == "subtraktion" or operator == "-":
-        result = sub(user_tal())
+    elif operator == "-":
+        result = sub(user_num())
         print(f"Resultatet blir: {result}")
-    elif operator == "gånger" or operator == "*":
-        result = mult(user_tal())
+    elif operator == "*":
+        result = mult(user_num())
         print(f"Resultatet blir: {result}")
     else:
-        result = div(user_tal())
-        print(f"Resultatet blir: {result}")
+        result = div(user_num())
+        if result == "ERROR! Ingen division med 0!":
+            print(result)
+        else:
+            print(f"Resultatet blir: {result}")
 
     again = str.lower(input("Vill du köra programmet en gång till? Ja / Nej "))
    
