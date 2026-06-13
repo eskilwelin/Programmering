027309c378252b3592235c8b0b137c30869588da
Eskil Welin <eskilwelin@gmail.com>
2026-06-13 12:37:52 +0200

    Ändrade win condition logiken, borde fungera korrekt nu.

--- hang_man/game.py
+++ hang_man/game.py
@@ -1,3 +1,10 @@
+"""
+TODO:
+- Ta bort debugg statements
+- Grafik
+
+"""
+
 import word_list
 
 word_to_guess = word_list.random_word()
@@ -13,7 +20,8 @@ print(" ".join(user_word))
 word_to_guess_letters = list(word_to_guess)
 print(word_to_guess_letters)
 
-while "_" in user_word and user_lives > 0: 
+
+while True: 
 
     print("Type a single letter!")
     user_guess = input("> ")
@@ -21,10 +29,9 @@ while "_" in user_word and user_lives > 0:
     if not user_guess.isalpha() or not len(user_guess) == 1:
         print("I said a single letter!")
     elif user_guess in already_guessed:
-        print(f"You already guessed {user_guess}")
+        print(f"You already tried {user_guess}")
     else:
-        count = 0
-        for i in word_to_guess_letters:
+        for counter, i in enumerate(word_to_guess_letters):
             if not user_guess in word_to_guess_letters:
                 user_lives -= 1
                 if user_lives > 1:
@@ -32,13 +39,21 @@ while "_" in user_word and user_lives > 0:
                     break
                 if user_lives > 0:
                     print(f"Guess again! You have {user_lives} try left!")
-                    break
-                print("You got hung!")
-                break
+                    break                
             if i == user_guess:
-                user_word[count] = user_guess
-            count += 1
-            
+                user_word[counter] = user_guess
+
         already_guessed.append(user_guess)
 
+    if not "_" in user_word:
+        solution = "".join(user_word)
+        print(f'You successfully guessed the word: "{solution}". You WIN!')
+        break
+
+    if user_lives == 0:
+        print("You got hung!")
+        break
+
     print(" ".join(user_word))
+
+
