131387821c519b53b96bcdc60f2edf7d8fd376cc
Eskil Welin <eskilwelin@gmail.com>
2026-06-13 14:09:07 +0200

    Snapshot innan jag reworkar logiken

--- hang_man/game.py
+++ hang_man/game.py
@@ -4,54 +4,50 @@ TODO:
 - Grafik
 
 """
-
 import word_list
 
 word_to_guess = word_list.random_word()
-print(word_to_guess)
 
 user_word = ["_"] * len(word_to_guess)
 
 user_lives = 6
-already_guessed = []
+already_guessed_letters = []
 
 print(" ".join(user_word))
 
 word_to_guess_letters = list(word_to_guess)
-print(word_to_guess_letters)
 
-
-while True: 
+run_game = True
+while run_game: 
 
     print("Type a single letter!")
-    user_guess = input("> ")
+    user_input_letter = input("> ")
 
-    if not user_guess.isalpha() or not len(user_guess) == 1:
+    if not user_input_letter.isalpha() or not len(user_input_letter) == 1:
         print("I said a single letter!")
-    elif user_guess in already_guessed:
-        print(f"You already tried {user_guess}")
+    elif user_input_letter in already_guessed_letters:
+        print(f"You already tried {user_input_letter}")
     else:
         for counter, i in enumerate(word_to_guess_letters):
-            if not user_guess in word_to_guess_letters:
+            if not user_input_letter in word_to_guess_letters:
                 user_lives -= 1
                 if user_lives > 1:
                     print(f"Guess again! You have {user_lives} tries left!")
                     break
-                if user_lives > 0:
+                if user_lives == 1:
                     print(f"Guess again! You have {user_lives} try left!")
-                    break                
-            if i == user_guess:
-                user_word[counter] = user_guess
+                    break            
+                if user_lives == 0:
+                    print("You got hung!")
+                    run_game = False    
+            if i == user_input_letter:
+                user_word[counter] = user_input_letter
+                already_guessed_letters.append(user_input_letter)
 
-        already_guessed.append(user_guess)
 
     if not "_" in user_word:
         solution = "".join(user_word)
         print(f'You successfully guessed the word: "{solution}". You WIN!')
-        break
-
-    if user_lives == 0:
-        print("You got hung!")
         break
 
     print(" ".join(user_word))
