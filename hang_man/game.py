acebc99703fad51923a17f5d4948698163be7ade
Eskil Welin <eskilwelin@gmail.com>
2026-06-13 10:44:50 +0200

    La till robustare input validation och koll för redan gissade bokstäver

--- hang_man/game.py
+++ hang_man/game.py
@@ -3,38 +3,42 @@ import word_list
 word_to_guess = word_list.random_word()
 print(word_to_guess)
 
-brackets = ["_"] * len(word_to_guess)
+user_word = ["_"] * len(word_to_guess)
 
-print(brackets)
 user_lives = 6
-print(user_lives)
+already_guessed = []
 
-print(" ".join(brackets))
+print(" ".join(user_word))
 
 word_to_guess_letters = list(word_to_guess)
 print(word_to_guess_letters)
 
-while "_" in brackets and user_lives > 0: 
+while "_" in user_word and user_lives > 0: 
 
     print("Type a single letter!")
     user_guess = input("> ")
+
     if not user_guess.isalpha() or not len(user_guess) == 1:
         print("I said a single letter!")
-
-    count = 0
-    for i in word_to_guess_letters:
-        if not user_guess in word_to_guess_letters:
-            user_lives -= 1
-            if user_lives > 1:
-                print(f"Guess again! You have {user_lives} tries left!")
+    elif user_guess in already_guessed:
+        print(f"You already guessed {user_guess}")
+    else:
+        count = 0
+        for i in word_to_guess_letters:
+            if not user_guess in word_to_guess_letters:
+                user_lives -= 1
+                if user_lives > 1:
+                    print(f"Guess again! You have {user_lives} tries left!")
+                    break
+                if user_lives > 0:
+                    print(f"Guess again! You have {user_lives} try left!")
+                    break
+                print("You got hung!")
                 break
-            if user_lives > 0:
-                print(f"Guess again! You have {user_lives} try left!")
-                break
-            print("You got hung!")
-            break
-        if i == user_guess:
-            brackets[count] = user_guess
-        count += 1
+            if i == user_guess:
+                user_word[count] = user_guess
+            count += 1
+            
+        already_guessed.append(user_guess)
 
-    print(" ".join(brackets))
+    print(" ".join(user_word))
